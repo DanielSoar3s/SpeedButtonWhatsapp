0505a3cfa4a5f9558167e34fb91c9180777d2b5f
Daniel Soares <danielsoaaraujo@hotmail.com>
2020-04-23 11:20:27 -0300

    adding buttonSlow

--- content.js
+++ content.js
@@ -11,9 +11,21 @@ const interval = setInterval(() => {
 
         button.addEventListener("click", () => {
             const audios = document.querySelectorAll('audio');
-            audios.forEach( (audio) => audio.playbackRate = 2 )
+            audios.forEach( (audio) => audio.playbackRate = 2 );
         });
 
+        const buttonSlow = document.createElement('button');
+        buttonSlow.innerHTML = '0.5x';
+        buttonSlow.classList.add('speedTwo');
+
+        buttonSlow.addEventListener("click", () => {
+            const audios = document.querySelectorAll('audio');
+            audios.forEach( (audio) => audio.playbackRate = 0.5 );
+        });
+
+        
+        header.appendChild(buttonSlow);
         header.appendChild(button);
+        
     }
 }, 1000);
